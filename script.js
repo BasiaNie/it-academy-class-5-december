7097dc64a8438733ceb22d8eb17ed92ec2281568
Basia Niedzielczyk <66676830+BasiaNie@users.noreply.github.com>
2020-12-05 11:34:46 +0100

    adding JS

---
+++ script.js
@@ -0,0 +1,18 @@
+var first = 5;
+var second = 7;
+
+var result = first + second;
+
+// alert('Hi');
+// popup 
+
+// console.log('Hi');
+
+// console.log(result);
+
+var name = 'John';
+var surname = 'Doe';
+
+var fullName = name + ' ' + surname;
+
+console.log(fullName);
